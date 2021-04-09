33010f5f932f98712d0fa322838864e84831d851
Zhang Huimeng <zhanghuimeng1997@gmail.com>
2021-04-09 23:33:20 +0800

    Added tag manipulation scripts

---
+++ tag_adder.py
@@ -0,0 +1,123 @@
+import argparse
+import collections
+import nltk
+import yaml
+import random
+import numpy as np
+
+Token = collections.namedtuple("Token", "token type name word_start")
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--corpus_file", type=str, help="input corpus file",
+)
+parser.add_argument(
+    "--config_file", type=str, help="config file",
+)
+parser.add_argument(
+    "--output_file", type=str, help="output file",
+)
+parser.add_argument(
+    "--whole_mask", action="store_true", help="output file",
+)
+args = parser.parse_args()
+
+with open(args.config_file, "r") as f:
+    my_dict = yaml.load(f, Loader=yaml.FullLoader)
+tag_name = []
+tag_prob = []
+tag_enclosed = []
+for k in my_dict["tag_info"]:
+    tag_name.append(k)
+    tag_prob.append(my_dict["tag_info"][k]["p"])
+    tag_enclosed.append(my_dict["tag_info"][k]["len"])
+M = len(tag_name)
+
+token_cnt_all = 0
+tag_cnt = [0] * M
+N = 0
+with open(args.corpus_file, "r") as f:
+    with open(args.output_file, "w") as fw:
+        for line in f:
+            tokens = line.rstrip().split(" ")
+
+            tokens = [Token(token=token, type="plain", name="",
+                            word_start=True if "▁" in token else False) for token in tokens]
+            L = len(tokens)
+            token_cnt_all += L
+
+            p0 = my_dict["tag_percent"]
+            p1 = 1 / (p0 * L + 1)
+            # trial to add tags (using geometry distribution)
+            while random.random() > p1:
+                # select tag by multinomial distribution
+                a = np.random.multinomial(1, tag_prob)
+                selected_tag_idx = -1
+                for i in range(M):
+                    if a[i] != 0:
+                        selected_tag_idx = i
+                        break
+
+                # try to insert a tag
+                all_enclose_trial_cnt = 0
+                while all_enclose_trial_cnt < 500:
+                    enclose_len = round(np.random.normal(tag_enclosed[selected_tag_idx]))
+                    if enclose_len > len(tokens):
+                        enclose_len = len(tokens)
+                    one_len_trail_cnt = 0
+                    while one_len_trail_cnt < 15:
+                        start = random.randint(0, len(tokens) - enclose_len)
+                        end = start + enclose_len
+                        # whole word masking
+                        if args.whole_mask:
+                            while start < len(tokens) and not tokens[start].word_start:
+                                start += 1
+                            while end < len(tokens) and not tokens[end].word_start:
+                                end += 1
+                            if start >= end or end > len(tokens):
+                                one_len_trail_cnt += 1
+                                continue
+                        # check open/close tags
+                        open_close_cnt = 0
+                        for i in range(start, end):
+                            if tokens[i].type == "start":
+                                open_close_cnt += 1
+                            elif tokens[i].type == "end":
+                                open_close_cnt -= 1
+                            if open_close_cnt < 0:
+                                break
+                        if open_close_cnt != 0:
+                            one_len_trail_cnt += 1
+                            continue
+                        new_tokens = []
+                        if start > 0:
+                            new_tokens += tokens[:start]
+                        new_tokens.append(
+                            Token(token="<%s>" % tag_name[selected_tag_idx], type="start",
+                                  name=tag_name[selected_tag_idx], word_start=True))
+                        new_tokens += tokens[start: end]
+                        new_tokens.append(
+                            Token(token="</%s>" % tag_name[selected_tag_idx], type="end",
+                                  name=tag_name[selected_tag_idx], word_start=True))
+                        if end < len(tokens):
+                            new_tokens += tokens[end:]
+                        tokens = new_tokens
+                        one_len_trail_cnt = -1
+                        break
+                    if one_len_trail_cnt == -1:
+                        tag_cnt[selected_tag_idx] += 1
+                        break
+                    else:
+                        all_enclose_trial_cnt += one_len_trail_cnt
+
+            tokens = [token.token for token in tokens]
+            fw.write(" ".join(tokens) + "\n")
+            N += 1
+
+            if N % 10000 == 0:
+                print("N = %d" % N)
+                print("Added tag percentage: %.02f%% (expected: %.02f%%)" % (sum(tag_cnt) / token_cnt_all * 100,
+                                                                             my_dict["tag_percent"] * 100))
+                for i in range(M):
+                    print("Tag <%s> percentage: %.02f%% (expected: %.02f%%)" % (tag_name[i], tag_cnt[i] / sum(tag_cnt) * 100,
+                                                                                tag_prob[i] * 100))
--- tag_statistics.py
+++ tag_statistics.py
@@ -2,21 +2,20 @@ import argparse
 import re
 import nltk
 import statistics
+import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--input_file", type=str, help="input file",
+)
+parser.add_argument(
+    "--output_file", type=str, help="output file",
 )
 args = parser.parse_args()
 
 with open(args.input_file, "r") as f:
     lines = f.readlines()
 lines = [line.strip() for line in lines]
-
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
 
 pattern = re.compile(r"<(/)?(\w*)>")
 
@@ -25,17 +24,18 @@ tag_cnt_dict = {}
 tag_cnt_all = 0  # all tags cnt
 token_cnt_all = 0  # all tags cnt
 for line in lines:
-    tokens = []
-    last = 0
-    r = pattern.search(line)
-    while r:
-        if r.start() > last:
-            tokens += nltk.word_tokenize(line[last: r.start() - 1])
-        tokens.append(r.group(0))
-        last = r.end()
-        r = pattern.search(line, last)
-    if last < len(line) - 1:
-        tokens += nltk.word_tokenize(line[last:])
+    # tokens = []
+    # last = 0
+    # r = pattern.search(line)
+    # while r:
+    #     if r.start() > last:
+    #         tokens += nltk.word_tokenize(line[last: r.start() - 1])
+    #     tokens.append(r.group(0))
+    #     last = r.end()
+    #     r = pattern.search(line, last)
+    # if last < len(line) - 1:
+    #     tokens += nltk.word_tokenize(line[last:])
+    tokens = line.split()
     token_cnt_all += len(tokens)
 
     # print(line)
@@ -43,7 +43,7 @@ for line in lines:
 
     tag_idx_dict = {}
     for i, token in enumerate(tokens):
-        r = pattern.match(token)
+        r = pattern.search(token)
         if r:
             content = r.group(2)
             tag_cnt_all += 1
@@ -65,3 +65,17 @@ print("Tag pairs / non_tags: %.02f%%" % (tag_cnt_all // 2 / (token_cnt_all - tag
 for k, v in tag_cnt_dict.items():
     print("%12s %7d (%6.02f%% enclosing len=%.02f)" % (k, v, v / tag_cnt_all * 100,
                                                        statistics.mean(tag_enclosing_len_dict[k])))
+
+output_dict = {}
+output_dict["token_cnt"] = token_cnt_all
+output_dict["tag_pair_cnt"] = tag_cnt_all // 2
+output_dict["tag_percent"] = tag_cnt_all // 2 / (token_cnt_all - tag_cnt_all)
+output_dict["tag_info"] = {}
+for k, v in tag_cnt_dict.items():
+    output_dict["tag_info"][k] = {}
+    output_dict["tag_info"][k]["cnt"] = v
+    output_dict["tag_info"][k]["p"] = v / tag_cnt_all
+    output_dict["tag_info"][k]["len"] = statistics.mean(tag_enclosing_len_dict[k])
+
+with open(args.output_file, "w") as yaml_file:
+    yaml.dump(output_dict, yaml_file, default_flow_style=False)
